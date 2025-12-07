3cda15734adfa1c30893f628fda9ba88d3067d5e
Wamp165 <oforment.cort@gmail.com>
2025-12-07 16:57:55 +0100

    Update Navbar component layout and styling; enhance menu structure with additional links

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,20 +11,19 @@ export default function Navbar({ slug, name }: NavbarProps) {
 
   return (
     <header className="w-full pt-10">
-      <nav className="max-w-7xl mx-auto text-[17px] font-light text-black">
+      <nav className="max-w-7xl mx-auto flex items-center text-[17px] font-light text-black">
 
-        {/* MENU*/}
-        <div className="flex justify-center items-center gap-40">
-
-          <Link to={`/${slug}`} className="hover:opacity-60 transition leading-tight">
+        {/* MENU */}
+        <div className="flex-1 flex justify-center gap-40">
+          <Link to={`/${slug}`} className="hover:opacity-60 transition -translate-y-2 relative font-medium">
             <div>{firstName}</div>
             <div>{lastName}</div>
           </Link>
-
-          <Link to={`/${slug}/projects`} className="hover:opacity-60 transition">Projects</Link>
+          <Link to={`/${slug}/projects`} className="hover:opacity-60 transition ">Projects</Link>
           <Link to={`/${slug}/series`} className="hover:opacity-60 transition">Series</Link>
           <Link to={`/${slug}/commercial`} className="hover:opacity-60 transition">Commercials</Link>
           <Link to={`/${slug}/contact`} className="hover:opacity-60 transition">Contact</Link>
+          <div/>
         </div>
 
       </nav>
