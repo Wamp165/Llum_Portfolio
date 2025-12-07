b6dcb1bde975ceee2061b152a5e92c69edbc7383
Wamp165 <oforment.cort@gmail.com>
2025-12-07 15:32:49 +0100

    Add Cloudinary integration and update user seeding with home banner

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -5,7 +5,8 @@ async function main() {
   await prisma.user.create({
     data: {
       name: "Zuzanna Basiukiewicz",
-      slug: "zuzanna"
+      slug: "zuzanna",
+      homeBanner: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765117248/_8310282_1-kopia_mllko3.jpg"
     }
   });
 
