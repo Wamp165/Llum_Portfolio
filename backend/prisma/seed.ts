e19a842b22f674b8bf51218c377bacc3024ed5a3
Wamp165 <oforment.cort@gmail.com>
2025-12-08 14:18:39 +0100

    Updating seed

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient, Category } from "@prisma/client";
+import { PrismaClient, Category, WorkSectionType } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
@@ -41,6 +41,94 @@ async function main() {
       category: Category.STORY,
       order: 2,
       userId: user.id,
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_LEFT_TEXT_RIGHT,
+      text: "We don’t discover anymore, everything we have to know is already served on the little phone in our pocket. We will just check “top 5 places you have to visit in… “, then we will check how everything looks so it won’t be a waste of time.",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199245/_9060074_gcei0z.jpg",
+      order: 1,
+      work: { connect: { id: 1 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_RIGHT_TEXT_LEFT,
+      text: "We go up the mountains in make-up, wearing the best sports clothes and then we still will correct pictures of us with filters. We will create a fake view, make it more spectacular.",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199249/_8310222_ptmwfs.jpg",
+      order: 2,
+      work: { connect: { id: 1 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_LEFT_TEXT_RIGHT,
+      text: "The series “transition” was created spontaneously. However, it reflects my all time thoughts.",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199546/4foto_sndmrc.jpg",
+      order: 1,
+      work: { connect: { id: 2 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_RIGHT_TEXT_LEFT,
+      text: "The time which is non stop passing, the part of when we were children, reckless.",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199548/3foto_go5nwx.jpg",
+      order: 2,
+      work: { connect: { id: 2 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_CENTER,
+      text: "In which moment we gave away these innocent time for responsibilities, work.",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199549/2foto_j5p2db.jpg",
+      order: 3,
+      work: { connect: { id: 2 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_ONLY,
+      text: "Where did the child we used to be?",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199550/5foto_c2bmdb.jpg",
+      order: 4,
+      work: { connect: { id: 2 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.TEXT_ONLY,
+      text: "This is a sample text, meant to test the only text field.",
+      order: 5,
+      work: { connect: { id: 2 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_LEFT_TEXT_RIGHT,
+      text: "I don't know what I want to say here yet. It won’t be long though ",
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199854/2foto_1_dbmtgq.jpg",
+      order: 1,
+      work: { connect: { id: 3 } },
+    },
+  });
+
+  await prisma.workSection.create({
+    data: {
+      type: WorkSectionType.IMAGE_ONLY,
+      image: "https://res.cloudinary.com/dr2l0nou2/image/upload/v1765199851/3foto_1_abfxta.jpg",
+      order: 2,
+      work: { connect: { id: 3 } },
     },
   });
 
