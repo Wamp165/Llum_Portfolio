eda107e895f8497de8270b154508fe023acdd875
Wamp165 <oforment.cort@gmail.com>
2025-12-22 20:48:58 +0100

    Refactor App component by removing unused public routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,15 @@ import { Routes, Route } from "react-router-dom";
 // Admin
 import AdminLogin from "./pages/admin/AdminLogin";
 import AdminDashboard from "./pages/admin/AdminDashboard";
+import RequireAuth from "./components/RequireAuth";
 
 // Public
-import HomePage from "./pages/HomePage";
-import ContactPage from "./pages/ContactPage";
-import RequireAuth from "./components/RequireAuth";
+
 
 function App() {
   return (
     <Routes>
       {/* ===== Public site ===== */}
-      <Route path="/" element={<HomePage />} />
-      <Route path="/contact" element={<ContactPage />} />
 
       {/* ===== Admin ===== */}
       <Route path="/admin/login" element={<AdminLogin />} />
